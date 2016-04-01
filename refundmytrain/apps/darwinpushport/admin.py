bf4eea19f0173797898a08e3919229961a5af780
Paul M Furley <paul@paulfurley.com>
2016-04-01 19:39:44 +0100

    Add CallingPoint and TimetableJourney
    and load_timetable_data command

--- refundmytrain/apps/darwinpushport/admin.py
+++ refundmytrain/apps/darwinpushport/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from refundmytrain.apps.darwinpushport.models import (
-    OperatingCompany, Location)
+    CallingPoint, OperatingCompany, Location, TimetableJourney)
 
 
 @admin.register(OperatingCompany)
@@ -17,6 +17,39 @@ class OperatingCompanyAdmin(admin.ModelAdmin):
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('tiploc', 'three_alpha', 'name')
     search_fields = list_display
+
+    def get_readonly_fields(self, request, obj=None):
+        return [f.name for f in self.model._meta.fields]
+
+
+class CallingPointInline(admin.TabularInline):
+    model = CallingPoint
+    extra = 0
+    can_delete = False
+
+    def get_readonly_fields(self, request, obj=None):
+        return [f.name for f in self.model._meta.fields]
+
+
+@admin.register(TimetableJourney)
+class TimetableJourneyAdmin(admin.ModelAdmin):
+    list_display = (
+        'rtti_train_id',
+        'start_date',
+        'start',
+        'end',
+        'operating_company',
+        'num_calling_points',
+        'train_uid',
+        'train_id',
+    )
+
+    search_fields = (
+        'rtti_train_id',
+        'operating_company__name',
+    )
+
+    inlines = [CallingPointInline]
 
     def get_readonly_fields(self, request, obj=None):
         return [f.name for f in self.model._meta.fields]
