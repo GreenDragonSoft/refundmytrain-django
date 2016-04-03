9eea5242110ef4aa6736f323ed22071a27633e66
Paul M Furley <paul@paulfurley.com>
2016-04-03 10:04:19 +0100

    FIXUPME importlog

--- refundmytrain/apps/darwinpushport/models.py
+++ refundmytrain/apps/darwinpushport/models.py
@@ -187,3 +187,6 @@ class ActualArrival(models.Model):
 class ImportLog(models.Model):
     filename = models.CharField(max_length=100)
     import_datetime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.filename
