b254398c87f61add347e68f375b9e7660a4ab51f
Paul M Furley <paul@paulfurley.com>
2016-04-05 08:46:37 +0100

    Use Europe/London timezone

--- refundmytrain/settings/common.py
+++ refundmytrain/settings/common.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = (
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
 LANGUAGE_CODE = 'en-gb'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
